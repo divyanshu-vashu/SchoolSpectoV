fc1f7e075e8230acd12f67e9349fa02d785b9e33
Divyanshu Singh <89933176+divyanshu-vashu@users.noreply.github.com>
2024-06-15 16:39:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Change this to 'dist' if Vercel expects this directory
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        // Remove 'bootstrap/dist/css/bootstrap.min.css' from externals
-      ],
-    },
-  },
-});
+})
